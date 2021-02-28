4a0c676ec9df16230cc7ac72cd2b78c23dd4358c
Raghavendra <raghuworld1992@outlook.com>
2021-02-28 22:32:38 +0530

    Making astronauts responsive

--- src/Components/Astronaut/SimpleAstronautCard.jsx
+++ src/Components/Astronaut/SimpleAstronautCard.jsx
@@ -6,7 +6,7 @@ import './Astronaut.css';
 const SimpleAstronautCard = ({ imageURL, text, nationality = null, href = '' }) => {
 
     const loadSimpleCard =
-        (<Card className='link'>
+        (<Card className='link' fluid>
             <Image className='astronautThumbNail' src={imageURL} wrapped ui={false} />
             <Card.Content textAlign='center' className='astronautContent'>
                 {text} {nationality ? ` (${nationality})` : null}
